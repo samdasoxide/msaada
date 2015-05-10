69c2e3fc03fece2464e6a7a2769d8a54071ecefe
samdasoxide <samdasoxide@gmail.com>
2015-05-10 04:28:11 +0300

    initial commit

--- routes.py
+++ routes.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask, render_template, url_for
 
 
@@ -37,5 +36,5 @@ def contacts():
 	return render_template('contacts.html')
 
 if __name__ == '__main__':
-	port = int(os.environ.get("PORT", 5000))
-	app.run(debug=True, host='0.0.0.0', port=port)
+	
+	app.run(debug=True)
